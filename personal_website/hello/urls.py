6824d298549fec871df86a863d564292136d78fa
gcasteleiro <57258734+gcasteleiro@users.noreply.github.com>
2021-10-21 23:49:34 -0400

    Page practice

---
+++ personal_website/hello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("gerry", views.gerry, name="gerry"),
+    path("david", views.david, name="david")
+]
