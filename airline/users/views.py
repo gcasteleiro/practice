1d48271fa399bae12c24091dfb85043c5f248a36
Gerry Casteleiro <g.casteleiro08@gmail.com>
2021-10-29 17:37:28 -0400

    Added more to users app

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
@@ -8,5 +9,20 @@ def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
 
-def login_request(request):
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid credentials"
+            })
+
     return render(request, "users/login.html")
+
+def logout_view(request):
+    pass
