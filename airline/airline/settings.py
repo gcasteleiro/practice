570cbe6df1e0b7c3138e0b65a31787049b89822e
Gerry Casteleiro <g.casteleiro08@gmail.com>
2021-10-29 13:36:27 -0400

    Deleted and started again

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-doo9z08o9n*kob7*87gk4hs-c!3no8wz!8z2ij5i@#e)uyzl3s'
+SECRET_KEY = 'django-insecure-=b#+cpg+7@d5cag0n8((f7$5s%y+cx-n2$lr4zsy+z(33xb+y*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'flights',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'flights'
 ]
 
 MIDDLEWARE = [
